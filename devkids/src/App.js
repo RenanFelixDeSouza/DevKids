a3c79ba5a2a51fbc00c2e9ea31d3d253a9de2406
RENAN FELIX DE SOUZA <96160923+redonpt@users.noreply.github.com>
2022-10-19 22:19:04 -0300

    atualizacao

--- devkids/src/App.js
+++ devkids/src/App.js
@@ -1,7 +1,17 @@
 import './App.css';
 
-import { BrowserRouter, Routes, Route, Navigate, Router } from 'react-router-dom'
-import Home from "./pages/Home/Home"
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+
+
+//hooks
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
+
+//context
+import {AuthProvider} from "./context/AuthContent"
+
+import Home from "./pages/Home/Home";
 import About from './pages/About/About';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -11,9 +21,30 @@ import Login from './pages/Login/login';
 //pages
 
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const {auth} = useAuthentication()
+
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+
+  }, [auth])
+
+  //barra de carregamento da página. usar algum efeito css diferente nessa parte
+  if(loadingUser) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
+     <AuthProvider value={user}>
+     <BrowserRouter>
       <Navbar/>
         <div className='container'>
           <Routes>
@@ -23,9 +54,9 @@ function App() {
             <Route path='/register' element={<Register/>}/>
           </Routes>
         </div>
-      </BrowserRouter>
       <Footer/>
-
+      </BrowserRouter>
+     </AuthProvider>
     </div>
   );
 }
